906acc9c75a71d882f5718993c2603ca88390fbe
trg222 <75914299+trg222@users.noreply.github.com>
2021-02-04 09:54:58 -0300

    proyecto node

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express ();
+const path = require ('path');
+
+
+
+app.get('/', function (req,res){
+res.sendFile(path.resolve(__dirname, './views', 'home.html'))
+}) 
+
+app.get('/signup', function (req,res){
+    res.sendFile(path.resolve(__dirname, './views', 'register.html'))
+    
+})
+
+app.get('/login', function (req,res){
+    res.sendFile(path.resolve(__dirname, './views', 'login.html'))
+    }) 
+app.use (express.static('public'))
+app.listen (app.set('puerto', process.env.PORT || 3000), ()=> console.log(`Servidor corriendo de manera satisfactoria  ${app.get('puerto')}` ));
+
+
+
+
+
